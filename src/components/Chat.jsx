1ad84828565556c99617d7e282d1d5d8464d2f19
Abhishek <abhishekrawat.1332003@gmail.com>
2023-04-09 14:17:16 +0530

    third

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,20 +2,24 @@ import React from 'react'
 import Cam from "../images/cam.png"
 import Add from "../images/add.png"
 import More from "../images/more.png"
+import Messages from './Messages'
+import Input from './Input'
 
 const Chat = () => {
   return (
-    <div className='chat'>
-    <div className="chatInfo">
-      <span>Jane</span>
-      <div className="chatIcons">
-        <img src={Cam} alt="" srcset="" />
-        <img src={Add} alt="" srcset="" />
-        <img src={More}alt="" srcset="" />
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Jane</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="" srcset="" />
+          <img src={Add} alt="" srcset="" />
+          <img src={More} alt="" srcset="" />
+        </div>
       </div>
+      <Messages/>
+      <Input/>
     </div>
-    </div>
-  )
+  );
 }
 
 export default Chat
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Person from '../images/Person.jpg'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+      <div className="messageInfo">
+        <img src={Person} alt="" />
+        <span>Just Now</span>
+      </div>
+      <div className="messageContent">
+      <p>hello</p>
+      <img src={Person} alt="" />                
+      </div>
+    </div>
   )
 }
 
