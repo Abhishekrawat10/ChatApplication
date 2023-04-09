3b52fb332944f54e52bce674473011535ee52849
Abhishek <abhishekrawat.1332003@gmail.com>
2023-04-09 18:54:10 +0530

    Fourth Commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.Api_Key,
+  authDomain: process.env.Auth_Domain,
+  projectId: process.env.Project_Id,
+  storageBucket: process.env.Storage_Bucket,
+  messagingSenderId: process.env.Messaging_Sender_Id,
+  appId: process.env.App_Id,
+};
+
+const app = initializeApp(firebaseConfig);
